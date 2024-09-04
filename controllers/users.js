23fb73961e282e97f9f894226e1145e1108df247
zainabdhaif <108354207+zainabdhaif@users.noreply.github.com>
2024-09-04 15:54:06 +0300

    added user contorller to create users

---
+++ controllers/users.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Something went wrong, try again.' });
+    }
+
+    const hashedPassword = bcrypt.hashSync(password, parseInt(process.env.SALT_ROUNDS));
+
+    const user = await User.create({ username, hashedPassword });
+    return res.status(201).json({ user});
+  } catch (error) {
+    res.status(400).json({ error: 'Something wen wrong, try again.' });
+  }
+});
+
+router.post('/signin', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const existingUser = await User.findOne({ username });
+
+    if (!existingUser) {
+      return res.status(400).json({ error: 'Invalid Credentials' });
+    }
+
+    const isValidPassword = bcrypt.compareSync(password, existingUser.hashedPassword);
+
+    if (!isValidPassword) {
+      throw Error('Invalid Credentials');
+    }
+
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+        username: existingUser.username,
+        type: existingUser.type
+      },
+      process.env.JWT_SECRET
+    );
+    return res.status(200).json({ user: existingUser, token });
+  } catch (error) {
+    res.status(400).json({ error: 'Something went wrong, try again.' });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ dotenv.config();
 require('./config/database');
 const express = require('express');
 
+//contorllers
+const usersRouter = require('./controllers/users');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,6 +18,8 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
+// Routes
+app.use('/users', usersRouter);
 
 app.listen(PORT, () => {
   console.log('The express app is ready!');
