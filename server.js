9520e414e28bf09d8ebd6443ab93f5b2b990c202
Maryam Qasim <maryam@DESKTOP-6B29GKT.>
2024-09-05 13:45:01 +0300

    meetup curd done

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const express = require('express');
 
 //contorllers
 const usersRouter = require('./controllers/users');
-
+const meetups = require('./controllers/meetups');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,6 +20,7 @@ app.use(express.json());
 
 // Routes
 app.use('/users', usersRouter);
+app.use('/meetups', meetups);
 
 app.listen(PORT, () => {
   console.log('The express app is ready!');
