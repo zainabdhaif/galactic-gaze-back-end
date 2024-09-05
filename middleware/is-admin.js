19e207a69d501e07e656ad54dbf23ed2d5e03b53
zainabdhaif <108354207+zainabdhaif@users.noreply.github.com>
2024-09-05 09:40:36 +0300

    added helpful middleware for verification and checking user role for authorization

---
+++ middleware/is-admin.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const isAdmin = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const tokenParts = token.split('.');
+    const tokenPayload = JSON.parse(Buffer.from(tokenParts[1], 'base64').toString());
+
+    if (tokenPayload.type === 'admin') {
+      next();
+    } else {
+        return res.status(401).json({ message: 'Not allowed to perform this action' });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json({ message: 'Invalid access' });
+  }
+};
+
+module.exports = isAdmin;
